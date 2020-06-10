51909a825c7c13b3cd2c0b39471698a60acb3667
othman19t <othman19t@gmail.com>
2020-06-09 22:03:43 -0700

    make it better

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Indisitionapp from "./components/indecisionApp";
+
+function App() {
+  return (
+    <div className="App">
+      <Indisitionapp />
+    </div>
+  );
+}
+
+export default App;
